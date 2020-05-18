ba660aaff5ee715c2f5217a22cec2cc220fade2d
pduren <57735971+kParkz@users.noreply.github.com>
2020-05-18 18:54:50 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "98.css";
+import styled from "styled-components";
+import "./styles/reset.css";
+
+const MainWrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+`;
+
+const WelcomeDiv = styled.div`
+  height: 10;
+  width: 40rem;
+  display: inline-block;
+  font-size: 1.2rem;
+  background: #c8c8c8;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainWrapper>
+      <WelcomeDiv className="window">
+        <div class="title-bar">
+          <div class="title-bar-text">xoxo</div>
+          <div class="title-bar-controls">
+            <button aria-label="Minimize"></button>
+            <button aria-label="Maximize"></button>
+            <button aria-label="Close"></button>
+          </div>
+        </div>
+        <div class="window-body">
+          <p>Welcome to Poke 98!</p>
+        </div>
+      </WelcomeDiv>
+    </MainWrapper>
   );
 }
 
