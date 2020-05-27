d55f3f1a9655178f1fabccb8bb400fa8164f1909
pduren <57735971+kParkz@users.noreply.github.com>
2020-05-27 13:03:46 -0500

    api requests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "98.css";
 import styled from "styled-components";
 import "./styles/reset.css";
+import axios from "axios";
 
 const MainWrapper = styled.div`
   height: 100vh;
@@ -10,29 +11,63 @@ const MainWrapper = styled.div`
 `;
 
 const WelcomeDiv = styled.div`
-  height: 10;
-  width: 40rem;
+  height: 10rem;
+  width: 60rem;
   display: inline-block;
-  font-size: 1.2rem;
+  font-size: 2rem;
   background: #c8c8c8;
 `;
 
 function App() {
+  const [pokedex, setPokedex] = useState([]);
+  const [pokemon, setPokemon] = useState({});
+
+  const pokeId = () => {
+    const min = Math.ceil(1);
+    const max = Math.floor(151);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/" + pokeId())
+      .then((response) => {
+        setPokemon(response.data);
+      })
+      .catch((error) => {
+        console.log("error!", error);
+      });
+  }, []);
+
   return (
     <MainWrapper>
-      <WelcomeDiv className="window">
-        <div class="title-bar">
-          <div class="title-bar-text">xoxo</div>
-          <div class="title-bar-controls">
-            <button aria-label="Minimize"></button>
-            <button aria-label="Maximize"></button>
-            <button aria-label="Close"></button>
+      <header>
+        <WelcomeDiv className="window">
+          <div class="title-bar">
+            <div class="title-bar-text">xoxo</div>
+            <div class="title-bar-controls">
+              <button aria-label="Minimize"></button>
+              <button aria-label="Maximize"></button>
+              <button aria-label="Close"></button>
+            </div>
           </div>
-        </div>
-        <div class="window-body">
-          <p>Welcome to Poke 98!</p>
-        </div>
-      </WelcomeDiv>
+          <div class="window-body">
+            <p>Welcome to Poke 98!</p>
+          </div>
+        </WelcomeDiv>
+      </header>
+      <section className="pokemon">
+        <img
+          src={
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+            pokemon.id +
+            ".png"
+          }
+          alt="Pokemon Sprite"
+        />
+        <h3>{pokemon.name}</h3>
+        {/* put search form here */}
+      </section>
     </MainWrapper>
   );
 }
