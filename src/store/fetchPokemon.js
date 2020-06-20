2a0b8755002f2f7313157a8e61de7f6c73c6ded2
Garrett Weems <gwgraphicdesign@gmail.com>
2020-06-19 22:29:45 -0500

    :sparkles: get all poke data

--- src/store/fetchPokemon.js
+++ src/store/fetchPokemon.js
@@ -15,8 +15,11 @@ const fetchPokemon = () => {
     // ! ONLY call api if you need to
     if (!loading && !pokemon && !error)
       Axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-        .then((res) => {
-          dispatch({ type: FETCH_POKEMON_SUCCESS, payload: res.data.results });
+        .then(async (res) => {
+          const pokemon = await Axios.all(res.data.results.map(getPokemonDetails));
+          console.log('pokemon: ', pokemon);
+
+          dispatch({ type: FETCH_POKEMON_SUCCESS, payload: pokemon });
         })
         .catch((err) => {
           dispatch({ type: FETCH_POKEMON_FAILURE });
@@ -25,5 +28,9 @@ const fetchPokemon = () => {
 
   return { loading, pokemon, error };
 };
+
+async function getPokemonDetails({ url, name }) {
+  return await Axios.get(url).then((res) => ({ name, ...res.data }));
+}
 
 export default fetchPokemon;
