db5c048404bc9a9bafaf27c4e0eeb95078f372d3
pduren <57735971+kParkz@users.noreply.github.com>
2020-06-24 13:00:33 -0500

    add and style details to detail page

--- src/pages/details.js
+++ src/pages/details.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import updatePokemon from "../store/updatePokemon";
+import { DetailCardWrapper, DetailWrapper, ContentHR } from "../styles/style";
+
 export default function PokemonDetailsPage() {
   const { loading, poke, error } = updatePokemon();
   const history = useHistory();
@@ -10,10 +12,21 @@ export default function PokemonDetailsPage() {
   if (loading) return <div>loading..</div>;
 
   if (error) return <div>error...</div>;
+  console.log("poke:", poke);
   return (
-    <div>
+    <DetailWrapper>
       <button onClick={handleClick}>back</button>
-      <div>{poke && poke.description}</div>
-    </div>
+      <DetailCardWrapper>
+        <img src={poke?.sprites.front_default} alt={poke?.name} />
+        <p>#{poke?.id}</p>
+        <ContentHR style={{ margin: "0 0" }} />
+        <h2>{poke?.name.charAt(0).toUpperCase() + poke?.name.slice(1)}</h2>
+        <ContentHR style={{ margin: "0 0" }} />
+        <p>{poke?.description}</p>
+        <p>
+          Height: {poke?.height / 10}m ✨✨✨ Weight: {poke?.weight / 10}kg
+        </p>
+      </DetailCardWrapper>
+    </DetailWrapper>
   );
 }
